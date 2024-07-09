ca49dbe73829714e5a1d8ae68ec2d86e42cafbd3
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-07-09 15:59:04 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240709/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void point10Stars(){
+        System.out.println("**********");
+    }
+    public static void main(String[] args) {
+        for(int i =0; i<5;i++){
+            point10Stars();
+        }
+    }
+}
