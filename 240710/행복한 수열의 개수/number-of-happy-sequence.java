b4b7fd76493c14f4ced324d628172a4b5e6f4df9
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-07-10 17:34:24 +0900

    Title: 행복한 수열의 개수, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240710/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240710/행복한 수열의 개수/number-of-happy-sequence.java
@@ -2,50 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static int n;
-    public static int checkRow(int[][] grid, int r, int m){
-        if(m == 1){
-            return 1;   
-        }
-        int sum = 0;
-        int cnt = 1;
+    public static int m;
+    public static int checkRow(int[][] grid, int r){
+        
+        int cnt = 0;
         int pre = grid[r][0];
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             if(pre == grid[r][i]){
                 cnt++;
                 if(cnt == m){
-                    sum++;
+                    return 1;
                 }
             }else{
                 pre = grid[r][i];
                 cnt =1;
             }
         }
-        return sum;
+        return 0;
     }
-    public static int checkCol(int[][] grid, int c, int m){
-        if( m == 1){
-            return 1;
-        }
-        int sum =0;
-        int cnt =1;
+    public static int checkCol(int[][] grid, int c){
+        
+        int cnt =0;
         int pre = grid[0][c];
         for(int i=1; i<n;i++){
             if (pre == grid[i][c]) {
                 cnt++;
                 if (cnt == m) {
-                    sum++;                    
+                    return 1;                   
                 }
             }else{
                 pre = grid[i][c];
                 cnt = 1;
             }
         }
-        return sum;
+        return 0;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        int m = sc.nextInt();
+        m = sc.nextInt();
 
         int[][] grid = new int[n][n];
         for(int i= 0 ;i<n; i++){
@@ -56,8 +51,8 @@ public class Main {
 
         int sum = 0;
         for(int i=0;i<n;i++){
-            int sum_r = checkRow(grid,i,m);
-            int sum_c = checkCol(grid,i,m);
+            int sum_r = checkRow(grid,i);
+            int sum_c = checkCol(grid,i);
             sum += sum_r + sum_c; 
         }
         System.out.println(sum);
