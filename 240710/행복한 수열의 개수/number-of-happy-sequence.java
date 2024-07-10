5609b8098ebcb53959742858d86f5cf8e6a4c4e3
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-07-10 17:38:00 +0900

    Title: 행복한 수열의 개수, Time: 304ms, Memory: 20MB, Status: Passed - CodeTree

--- 240710/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240710/행복한 수열의 개수/number-of-happy-sequence.java
@@ -1,61 +1,61 @@
 import java.util.Scanner;
 
-public class Main {
-    public static int n;
-    public static int m;
-    public static int checkRow(int[][] grid, int r){
-        
-        int cnt = 0;
-        int pre = grid[r][0];
-        for(int i=0;i<n;i++){
-            if(pre == grid[r][i]){
-                cnt++;
-                if(cnt == m){
-                    return 1;
+    public class Main {
+        public static int n;
+        public static int m;
+        public static int checkRow(int[][] grid, int r){
+            
+            int cnt = 0;
+            int pre = grid[r][0];
+            for(int i=0;i<n;i++){
+                if(pre == grid[r][i]){
+                    cnt++;
+                    if(cnt == m){
+                        return 1;
+                    }
+                }else{
+                    pre = grid[r][i];
+                    cnt =1;
                 }
-            }else{
-                pre = grid[r][i];
-                cnt =1;
             }
+            return 0;
         }
-        return 0;
-    }
-    public static int checkCol(int[][] grid, int c){
-        
-        int cnt =0;
-        int pre = grid[0][c];
-        for(int i=1; i<n;i++){
-            if (pre == grid[i][c]) {
-                cnt++;
-                if (cnt == m) {
-                    return 1;                   
+        public static int checkCol(int[][] grid, int c){
+            
+            int cnt =0;
+            int pre = grid[0][c];
+            for(int i=0; i<n;i++){
+                if (pre == grid[i][c]) {
+                    cnt++;
+                    if (cnt == m) {
+                        return 1;                   
+                    }
+                }else{
+                    pre = grid[i][c];
+                    cnt = 1;
                 }
-            }else{
-                pre = grid[i][c];
-                cnt = 1;
             }
+            return 0;
         }
-        return 0;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            n = sc.nextInt();
+            m = sc.nextInt();
 
-        int[][] grid = new int[n][n];
-        for(int i= 0 ;i<n; i++){
-            for(int j=0;j<n;j++){
-                grid[i][j] = sc.nextInt();
+            int[][] grid = new int[n][n];
+            for(int i= 0 ;i<n; i++){
+                for(int j=0;j<n;j++){
+                    grid[i][j] = sc.nextInt();
+                }
             }
-        }
 
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            int sum_r = checkRow(grid,i);
-            int sum_c = checkCol(grid,i);
-            sum += sum_r + sum_c; 
+            int sum = 0;
+            for(int i=0;i<n;i++){
+                int sum_r = checkRow(grid,i);
+                int sum_c = checkCol(grid,i);
+                sum += sum_r + sum_c; 
+            }
+            System.out.println(sum);
+            sc.close();
         }
-        System.out.println(sum);
-        sc.close();
     }
-}
