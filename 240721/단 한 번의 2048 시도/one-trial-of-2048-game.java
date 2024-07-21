37c183152213f7a9231c111e6e0c8905201d6b01
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-07-21 16:27:32 +0900

    Title: 단 한 번의 2048 시도, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240721/단 한 번의 2048 시도/one-trial-of-2048-game.java
+++ 240721/단 한 번의 2048 시도/one-trial-of-2048-game.java
@@ -4,205 +4,112 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
-    private static int[][] grid = new int[4][4];
-    private static String dir;
+    private static int n = 4;
+    private static int[][] grid = new int[n][n];
+    private static int[][] newGrid = new int[n][n];
 
-    private static void goleft(){
-        //한 가로줄씩 봐야돼.
-        for(int i=0;i<4;i++){
-            //일단 공백이 있는 지없는지 먼저 봐야할것같은데...
-            //이제 왼쪽으로밀어
-            int[] tmp1 = new int[4];
-            int endOfTmp1 =0;
-            for(int j=0;j<4;j++){
-                if (grid[i][j] != 0) {
-                    tmp1[endOfTmp1++] = grid[i][j];
-                }
-            }
-
-            //이제 옮겨
-            for(int j=0;j<4;j++){
-                grid[i][j] = tmp1[j];
-            }
-            //벽쪽에서부터 같은 수가 2개 있는가 없을때까지 돌려봐야할것같은데.
-            for(int curIdx = 0; curIdx<3;curIdx++){
-                if(grid[i][curIdx] == 0){
-                    continue;
-                }
-                if (grid[i][curIdx] == grid[i][curIdx+1]) {
-                    grid[i][curIdx] = 2*grid[i][curIdx];
-                    grid[i][curIdx+1] = 0;
-                }
-            }
-
-            //이제 왼쪽으로밀어
-            int[] tmp = new int[4];
-            int endOfTmp =0;
-            for(int j=0;j<4;j++){
-                if (grid[i][j] != 0) {
-                    tmp[endOfTmp++] = grid[i][j];
-                }
-            }
-
-            //이제 옮겨
-            for(int j=0;j<4;j++){
-                grid[i][j] = tmp[j];
-            }
-        }
-    }
-    private static void goright(){
-        //가로줄 보자
-        for(int i=0;i<4;i++){
-             //오른쪽으로 이제 밀기
-             int[] tmp1 = new int[4];
-             int endOfTmp1 = 3;
-             for(int j=3; j>=0; j--){
-                 if (grid[i][j] !=0) {
-                     tmp1[endOfTmp1--] =grid[i][j];
-                 }
-             }
-             // 다시 옮기기
-             for(int j=0;j<4;j++){
-                 grid[i][j] = tmp1[j];
-             }
-            //벽면 오른쪽이니깐 cur을 3부터 0까지
-            for(int curIdx =3 ; curIdx>0 ; curIdx--){
-                if (grid[i][curIdx] == 0) {
-                    continue;
-                }
-                if (grid[i][curIdx] == grid[i][curIdx-1]) {
-                    grid[i][curIdx] = 2*grid[i][curIdx];
-                    grid[i][curIdx-1] = 0;
-                }
-            }
-            //오른쪽으로 이제 밀기
-            int[] tmp = new int[4];
-            int endOfTmp = 3;
-            for(int j=3; j>=0; j--){
-                if (grid[i][j] !=0) {
-                    tmp[endOfTmp--] =grid[i][j];
-                }
-            }
-            // 다시 옮기기
-            for(int j=0;j<4;j++){
-                grid[i][j] = tmp[j];
-            }
-        }
-    }
-    private static void goup(){
-        //세로줄
-        for(int j=0;j<4;j++){
-            
-            //위로 밀기
-            int[] tmp1 = new int[4];
-            int endOfTmp1 = 0;
-            for(int i=0;i<4;i++){
-                if (grid[i][j] != 0) {
-                    tmp1[endOfTmp1++] = grid[i][j];
-                }
-            }
-            //copy
-            for(int i=0;i<4;i++){
-                grid[i][j] = tmp1[i];
-            }
-            //위가 벽면이다.
-            for(int curIdx =0; curIdx<3; curIdx++){
-                if (grid[curIdx][j] == 0) {
-                    continue;
-                }
-                if (grid[curIdx][j] == grid[curIdx+1][j]) {
-                    grid[curIdx][j] = 2*grid[curIdx][j];
-                    grid[curIdx+1][j] = 0; 
-                }
-            }
-
-            //위로 밀기
-            int[] tmp = new int[4];
-            int endOfTmp = 0;
-            for(int i=0;i<4;i++){
-                if (grid[i][j] != 0) {
-                    tmp[endOfTmp++] = grid[i][j];
-                }
-            }
-            //copy
-            for(int i=0;i<4;i++){
-                grid[i][j] = tmp[i];
-            }
-        }
-    }
-    private static void godown(){
-        //세로줄
-        for(int j=0;j<4;j++){
-            
-            //밑으로 밀기
-            int[] tmp1 = new int[4];
-            int endOfTmp1 = 3;
-            for(int i=3;i>=0;i--){
-                if (grid[i][j] !=0) {
-                    tmp1[endOfTmp1--] = grid[i][j];
-                }
-            }
-            //copy
-            for(int i=0;i<4;i++){
-                grid[i][j] = tmp1[i];
-            }
-            //벽이 밑이다.
-            for(int curIdx = 3; curIdx>0; curIdx--){
-                if (grid[curIdx][j] == 0) {
-                    continue;
-                }
-
-                if (grid[curIdx][j] == grid[curIdx-1][j]) {
-                    grid[curIdx][j] = 2*grid[curIdx][j];
-                    grid[curIdx-1][j] = 0;
-                }
-            }
-
-            //밑으로 밀기
-            int[] tmp = new int[4];
-            int endOfTmp = 3;
-            for(int i=3;i>=0;i--){
-                if (grid[i][j] !=0) {
-                    tmp[endOfTmp--] = grid[i][j];
-                }
-            }
-            //copy
-            for(int i=0;i<4;i++){
-                grid[i][j] = tmp[i];
-            }
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        for(int i=0;i<4;i++){
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<4; j++){
+            for(int j=0; j<n;j++){
                 grid[i][j] = Integer.parseInt(st.nextToken());
-            }    
+            }
         }
 
-        dir = br.readLine();
-        if  (dir.equals("L")){
-            goleft();
-        }
-        else if(dir.equals("R")){
-            goright();
-        }
-        else if(dir.equals("U")){
-            goup();
-        }
-        else if(dir.equals("D")){
-            godown();            
-        }
+        char dir = br.readLine().charAt(0);
+        int[] dirMapper = new int[4];
+        dirMapper['D'] = 0;
+        dirMapper['R'] = 1;
+        dirMapper['U'] = 2;
+        dirMapper['L'] = 3;
+        
+        titlt(dirMapper[dir]);
 
-        for(int i=0;i<4;i++)
-        {
-            for(int j=0; j<4; j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
                 System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }
+    }
+    private static void initNewGrid(){
+        for(int i=0;i<n;i++){
+            for(int j=0; j<n; j++){
+                newGrid[i][j] = 0;
+            }
+        }
+    }
+    private static void titlt(int moveDir){
+        // movedir만큼 90도 시계방향 회전을 한다.
+        for(int i=0;i<moveDir;i++){
+            rotate();
+        }
+        // 그러면 다 밑으로 drop시키면 된다는 소리.
+        drop();
+        // 다시 원상태로 돌리면 된다. 총 360도 돌리면 되니깐 4-movedir번 돌려
+        for(int i=0; i<4-moveDir ; i++){
+            rotate();
+        }
+    }
+    private static void rotate(){
+        initNewGrid();
+
+        // 90도 시계방향으로 돌려.
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                newGrid[i][j] = grid[3-j][i];
+            }
+        }
+        // 다시 grid에 옮겨.
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                grid[i][j] = newGrid[i][j];
+            }
+        }
+    }
+
+    private static void drop(){
+        for (int j=0;j<n;j++){
+             //밑으로 밀기
+             int[] tmp1 = new int[4];
+             int endOfTmp1 = 3;
+             for(int i=3;i>=0;i--){
+                 if (grid[i][j] !=0) {
+                     tmp1[endOfTmp1--] = grid[i][j];
+                 }
+             }
+             //copy
+             for(int i=0;i<4;i++){
+                 grid[i][j] = tmp1[i];
+             }
+             //벽이 밑이다.
+             for(int curIdx = 3; curIdx>0; curIdx--){
+                 if (grid[curIdx][j] == 0) {
+                     continue;
+                 }
+ 
+                 if (grid[curIdx][j] == grid[curIdx-1][j]) {
+                     grid[curIdx][j] = 2*grid[curIdx][j];
+                     grid[curIdx-1][j] = 0;
+                 }
+             }
+ 
+             //밑으로 밀기
+             int[] tmp = new int[4];
+             int endOfTmp = 3;
+             for(int i=3;i>=0;i--){
+                 if (grid[i][j] !=0) {
+                     tmp[endOfTmp--] = grid[i][j];
+                 }
+             }
+             //copy
+             for(int i=0;i<4;i++){
+                 grid[i][j] = tmp[i];
+             }
+        }
+
     }
 }
