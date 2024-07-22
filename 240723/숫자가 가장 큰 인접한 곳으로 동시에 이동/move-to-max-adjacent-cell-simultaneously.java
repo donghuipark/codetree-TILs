cf1f5ba5ecccd1deedc580d37b4c131105b81a85
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-07-23 01:05:12 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
+++ 240723/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -1,62 +1,122 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static int[][] map, direction = {{0,1},{0,-1},{1,0},{-1,0}};
-    public static int n, MAX_LEN = 21;
-    public static Queue<int[]> queue = new ArrayDeque<>();
-    public static void main(String[] args) throws IOException{
-        // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken()), t = Integer.parseInt(st.nextToken());
-        map = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int r = Integer.parseInt(st.nextToken())-1, c = Integer.parseInt(st.nextToken())-1;
-            queue.offer(new int[]{r, c});
-        }
-        for(int i=0; i<t; i++) {
-            move();
-        }
-        System.out.print(queue.size());
-        br.close();
+class Pair { 
+    int x, y;
+    public Pair(int x, int y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
+}
+public class Main{
+    private static int n,m,t;
+    private static int[][] arr;
+    private static int[][] count;
+    private static int[][] nextCount;
+
+    private static boolean inRange(int x, int y) {
+        return 1 <= x && x <= n && 1 <= y && y <= n;
     }
-
-    public static void move() {
-        Set<Integer> set = new HashSet<>(), removes = new HashSet<>();
-        int size = queue.size();
-        for(int i=0; i<size; i++) {
-            int[] pos = queue.poll();
-
-            int r=0, c=0, max = -1;
-            for(int j=0; j<direction.length; j++) {
-                int nextR = pos[0] + direction[j][0];
-                int nextC = pos[1] + direction[j][1];
-                if(isRange(nextR, nextC) && map[nextR][nextC] >= max) {
-                    r = nextR;
-                    c = nextC;
-                    max = map[r][c];
+    private static void simulate(){
+        //움직이기
+        moveAll();
+        //remove
+        remove();
+    }
+    private static void moveAll(){
+        nextCount = new int[n+1][n+1];
+        //nextCount init
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=n;j++){
+               nextCount[i][j] =0;
+            }
+        }
+        //구슬 있으면 move해보기
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=n; j++){
+                if (count[i][j] == 1) {
+                    move(i, j);                    
                 }
             }
-            int key = r*MAX_LEN + c;
-            if(set.contains(key)) removes.add(key);
-            set.add(key);
-            queue.offer(new int[]{r, c});
         }
-        size = queue.size();
-        for(int i=0; i<size; i++) {
-            int[] pos = queue.poll();
-            if(removes.contains(pos[0]*MAX_LEN + pos[1])) continue;
-            queue.offer(pos);
+        //nextCount값을 count에 copy
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                count[i][j] = nextCount[i][j];
+            }
         }
     }
+    private static void move(int x, int y){
+        Pair nextPos = getMaxNeighborPos(x,y);
+        int nextX = nextPos.x, nextY = nextPos.y;
 
-    public static boolean isRange(int r, int c) {
-        return r>=0 && r<n && c>=0 && c<n;
+        nextCount[nextX][nextY] += 1;
+    }
+    
+    private static Pair getMaxNeighborPos(int curX, int curY){
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0,0,-1,1};
+
+        int maxNum =0;
+        Pair maxPos = new Pair(-1, -1);
+
+        for(int i=0;i<4;i++){
+            int nextX = curX + dx[i];
+            int nextY = curY + dy[i];
+
+            if (inRange(nextX, nextY) && arr[nextX][nextY] > maxNum) {
+                maxNum = arr[nextX][nextY];
+                maxPos = new Pair(nextX, nextY);
+            }
+        }
+        return maxPos;
+    }
+    private static void remove(){
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=n ;j++){
+                if (count[i][j] >= 2) {
+                    count[i][j] =0;                    
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());    
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+
+        arr = new int[n+1][n+1];
+        count = new int[n+1][n+1];
+
+        for(int i=1;i<=n;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=n;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=1;i<=m;i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            count[x][y] = 1;
+        }
+
+        while (t-- >0) {
+            simulate();
+        }
+        int ans = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                ans += count[i][j];
+            }
+        }
+        System.out.println(ans);
     }
 }
