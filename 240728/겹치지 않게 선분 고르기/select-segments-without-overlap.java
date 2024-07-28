356d29ddd15d462c85193dd45b0487ab48ae6c03
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-07-28 15:35:49 +0900

    Title: 겹치지 않게 선분 고르기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240728/겹치지 않게 선분 고르기/select-segments-without-overlap.java
+++ 240728/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main{
     private static int n;
-    private static int ans = 1;
+    private static int ans = 0;
     private static int[][] line;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -27,22 +27,20 @@ public class Main{
         Arrays.sort(line, new Comparator<int[]>() { // new Comparator 함수 Override
             @Override
             public int compare(int[] o1, int[] o2) {
-                if(o1[0] == o2[0]){ // 첫번째 원소의 값이 같을 때 두번쨰 원소의 값을 기준으로 정렬
-                    return o1[1] - o2[1];
-                }else{ // 같지 않을 시 첫번째 원소를 기준으로 정렬
-                    return o1[0] - o2[0];
-                }
+              
+				// 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.  
+				if(o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}
+				return o1[1] - o2[1];
             }
         });
         
         int lineSize = line.length;
-        // 시작점
-        int start = line[0][0];
-        int end = line[0][1];
-        for(int i=1;i<lineSize;i++){
+        int end = 0;
+        for(int i=0;i<lineSize;i++){
             if (end < line[i][0]) {
                 ans++;
-                start = line[i][0];
                 end = line[i][1];
             }
         }
