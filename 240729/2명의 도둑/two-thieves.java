e42cec3967c10c150dee6b74f25f8baa8740c565
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-07-29 20:21:28 +0900

    Title: 2명의 도둑, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240729/2명의 도둑/two-thieves.java
+++ 240729/2명의 도둑/two-thieves.java
@@ -1,152 +1,113 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+    private static int n,m,c;
+    private static int ans, maxValue;
+    private static int[][] grid;
+    private static int[][] memo;
+    private static int[] stolen;
 
-    static int N;
-    static int M;
-    static int C;
-    static int[][] grid;
-    static int partialMaxValue;
-    static int maxValue;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
-        grid = new int[N][N];
+        grid = new int[n][n];
+        memo = new int[n][n];
+        stolen = new int[m];
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<n;j++){
                 grid[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        boolean[][] isStolen = new boolean[N][N];
-        List<int[]> steals = new ArrayList<>();
-        findMaxValue(0, isStolen, steals);
+        //dp 초기화
+        for(int sx = 0; sx<n; sx++){
+            for(int sy =0; sy<n; sy++){
+                memo[sx][sy] = -1;
+            }
+        }
 
-        sb.append(maxValue);
-
-        bw.write(sb.toString());
-        bw.close();
-        br.close();
+        // 1번 도둑 (sx1, sy1) ~ (sx1, sy1+m-1) 까지 훔치기
+        // 2번 도둑 (sx2, sy2) ~ (sx2, sy2+m-1) 까지 훔치기
+        for(int sx1 =0; sx1<n; sx1++){
+            for(int sy1 =0; sy1<n; sy1++){
+                for(int sx2 =0; sx2<n; sx2++){
+                    for(int sy2=0; sy2<n; sy2++){
+                        // 일단 1번, 2번도둑이 제대로 된 곳에서 끝나는지
+                        // 그리고 겹치지 않았는지 확인해야한다.
+                        if (possible(sx1, sy1, sx2, sy2)) {
+                            ans = Math.max(ans, findMax(sx1, sy1) + findMax(sx2, sy2));
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(ans);
     }
 
-    private static void findMaxValue(int depth, boolean[][] isStolen, List<int[]> steals) {
-        if (depth == 2) {
-            int value = 0;
-            for (int[] steal : steals) {
-                if (canSteal(steal)) {
-                    value += getValue(steal);
-                    continue;
-                }
-                int targetDepth = 0;
-                partialMaxValue = 0;
+    private static boolean possible(int x1, int y1, int x2, int y2){
+        // y1, y2가 격자밖이면 false
+        if (y1+m-1 >= n || y2+m-1 >= n) {
+            return false;
+        }
 
-                while (++targetDepth < M) {
-                    List<Integer> partials = new ArrayList<>();
-                    findPartialMaxValue(0, targetDepth, 0, 0, steal, partials);
-                }
-                value += partialMaxValue;
+        // row가 다르면 true
+        if (x1 != x2) {
+            return true;
+        }
+        else{ // 같은 row일땐 겹치지는지 확인해야한다.
+            if (!(y1+m-1 < y2 || y1 > y2-m+1)) {
+                return false;
             }
-            maxValue = Math.max(maxValue, value);
+        }
+
+        return true;
+    }
+
+    // (x, y) ~ (x, y+m-1) 까지 숫자중 적절하게 고른다음.
+    // 합이 용량을 넘기않으면 최대값으로 배출
+    private static int findMax(int x, int y){
+        //memo에 값이 있다면 그거 반환
+        if (memo[x][y] != -1) {
+            return memo[x][y];
+        }
+
+        // y1~y1+m-1까지 훔친거 저장하는해야돼.
+        for(int i = y; i<= y + m-1; i++){
+            stolen[i-y] = grid[x][i];
+        }
+
+        maxValue =0;
+        findMaxSum(0, 0, 0);
+
+        memo[x][y] = maxValue;
+        return maxValue;
+    }
+    //시작점으로부터 m까지 선택할지 안할지에 대해서 ..?
+    private static void findMaxSum(int depth, int currWeight, int currVal){
+        if (depth == m) {
+            if(currWeight <= c){
+                maxValue = Math.max(maxValue, currVal);
+            }
             return;
         }
+        
+        // currIdx index 에 있는 숫자를 선택하지 않은 경우
+        findMaxSum(depth + 1, currWeight, currVal);
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N - M + 1; j++) {
-                if (isUsed(isStolen, i, j)) {
-                    continue;
-                }
-                int[] steal = getSteal(i, j);
-                markSteal(isStolen, i, j, true);
-                steals.add(steal);
-
-                findMaxValue(depth + 1, isStolen, steals);
-
-                markSteal(isStolen, i, j, false);
-                steals.remove(steal);
-            }
-        }
-    }
-
-    private static boolean isUsed(boolean[][] isStolen, int x, int y) {
-        for (int j = y; j < y + M; j++) {
-            if (isStolen[x][j]) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean canSteal(int[] steal) {
-        int sum = 0;
-        for (int st : steal) {
-            sum += st;
-        }
-        return sum <= C;
-    }
-
-    private static boolean canPartialSteal(List<Integer> partials) {
-        int sum = 0;
-        for (Integer part : partials) {
-            sum += part;
-        }
-        return sum <= C;
-    }
-
-    private static void markSteal(boolean[][] isStolen, int x, int y, boolean flag) {
-        for (int j = y; j < y + M; j++) {
-            isStolen[x][j] = flag;
-        }
-    }
-
-    private static int[] getSteal(int x, int y) {
-        int[] steal = new int[M];
-        for (int j = y; j < y + M; j++) {
-            steal[j - y] = grid[x][j];
-        }
-        return steal;
-    }
-
-    private static void findPartialMaxValue(int depth, int targetDepth, int idx, int sum, int[] steal, List<Integer> partials) {
-        if (depth == targetDepth && canPartialSteal(partials)) {
-            partialMaxValue = Math.max(partialMaxValue, getPartialValue(partials));
-            return;
-        }
-
-        for (int i = idx; i < steal.length; i++) {
-            Integer part = steal[i];
-            if (sum + part > C) {
-                continue;
-            }
-            partials.add(part);
-            findPartialMaxValue(depth + 1, targetDepth, i + 1, sum + part, steal, partials);
-            partials.remove(part);
-        }
-    }
-
-    private static int getPartialValue(List<Integer> partials) {
-        int value = 0;
-        for (Integer partial : partials) {
-            value += partial * partial;
-        }
-        return value;
-    }
-
-    private static int getValue(int[] steal) {
-        int value = 0;
-        for (int i = 0; i < steal.length; i++) {
-            value += steal[i] * steal[i];
-        }
-        return value;
+        // currIdx index 에 있는 숫자를 선택한 경우
+        // 무게는 a[currIdx] 만큼 늘지만
+        // 문제 정의에 의해 가치는 a[currIdx] * a[currIdx] 만큼 늘어납니다.
+        findMaxSum(depth + 1, currWeight + stolen[depth], 
+                   currVal + stolen[depth] * stolen[depth]);
     }
 }
