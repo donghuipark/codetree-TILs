e8dde6e2682b2a0bfbf6ec46e2d650c1f1f10445
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-06 14:33:01 +0900

    Title: 사각형 채우기 3, Time: 111ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240806/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX_NUMBER = 1001;
+    static final int MOD = 1_000_000_007;
+    static int n;
+    static long[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        dp = new long[MAX_NUMBER];
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
+            for(int j = i - 3; j >= 0; j--)
+                dp[i] = (dp[i] + dp[j] * 2) % MOD;
+        }
+        
+        sb.append(dp[n]);
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+}
