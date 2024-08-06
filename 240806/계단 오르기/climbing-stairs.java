6be1db1acd6b6da52796be829dedee2e9cdbee08
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-06 10:46:45 +0900

    Title: 계단 오르기, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240806/계단 오르기/climbing-stairs.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static int n;
+    private static int[] dp;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[1001];
+
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for(int i=4; i<=n; i++){
+            dp[i] = dp[i-2] + dp[i-3];
+        }
+
+        System.out.println(dp[n]);
+
+
+    }
+}
