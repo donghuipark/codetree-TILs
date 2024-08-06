87304ade86c371228bb3ca928303b4b29b32a11a
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-06 15:36:12 +0900

    Title: 서로 다른 BST 개수 세기, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

--- 240806/서로 다른 BST 개수 세기/number-of-unique-bst.java
+++ 240806/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -13,9 +13,8 @@ public class Main {
         dp = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
         
-        for (int i=3; i<=n; i++) {
+        for (int i=2; i<=n; i++) {
             for (int j=1; j<=i; j++) {
                 int left = j - 1;
                 int right = i - j;
