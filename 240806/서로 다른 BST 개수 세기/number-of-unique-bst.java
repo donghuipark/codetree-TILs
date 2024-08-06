f8c8c2b5b7ba71b05276792e6e469f482000ae23
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-06 15:35:00 +0900

    Title: 서로 다른 BST 개수 세기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240806/서로 다른 BST 개수 세기/number-of-unique-bst.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static int n;
+    private static int[] dp;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i=3; i<=n; i++) {
+            for (int j=1; j<=i; j++) {
+                int left = j - 1;
+                int right = i - j;
+                dp[i] = dp[i] + (dp[left] * dp[right]);
+            }
+        }
+       System.out.println(dp[n]);
+        
+        
+    }
+}
