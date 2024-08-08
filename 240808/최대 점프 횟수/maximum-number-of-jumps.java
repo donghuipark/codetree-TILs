c5334dc5713a25c5201ba18d2aede669df803eea
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-08 20:58:01 +0900

    Title: 최대 점프 횟수, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240808/최대 점프 횟수/maximum-number-of-jumps.java
+++ 240808/최대 점프 횟수/maximum-number-of-jumps.java
@@ -20,11 +20,14 @@ public class Main {
         for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        
         for(int i=0;i<n;i++){
             dp[i] = 0;
 
             for(int j=0;j<i;j++){
+                if (dp[j] == 0) {
+                    continue;
+                }
                 if (i <= j + arr[j]) {
                     dp[i] = Math.max(dp[i], dp[j]+1);
                 }
