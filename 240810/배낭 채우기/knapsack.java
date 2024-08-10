deb0292e6e1a69dfcafacdb88fc957a837a29c0d
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-10 17:00:19 +0900

    Title: 배낭 채우기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240810/배낭 채우기/knapsack.java
+++ 240810/배낭 채우기/knapsack.java
@@ -4,41 +4,51 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int n,m;
-    private static int[] bag;
-    private static int[][] jewel;
+    private static int n, m;
+    private static int[] weight;
+    private static int[] value;
+    private static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        //보석 weight, value 알려주기
         n = Integer.parseInt(st.nextToken());
+        // 총 무게 == 원하는 무게.
         m = Integer.parseInt(st.nextToken());
 
-        bag = new int[m+1];
-        jewel = new int[n][2];
+        weight = new int[n+1];
+        value = new int[n+1];
 
-        for(int i=0;i<n;i++){
+        for(int i=1;i<=n;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<2;j++){
-                jewel[i][j] = Integer.parseInt(st.nextToken());
-            }    
+            weight[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
         }
 
-        // for(int i=0;i<=m;i++){
-        //     bag[i] = Integer.MIN_VALUE;
-        // }
-        // bag[0] = 0;
+        //dp만들기
+        dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                dp[i][j] = Integer.MIN_VALUE;
+            }
+        }
+        dp[0][0] = 0;
 
-        for(int i=0;i<n;i++){//jewel weight, value
-            for(int j=m;j>=0;j--){//bag weight
-                if (j >= jewel[i][0]) {
-                    //if (bag[j-jewel[i][0]] != Integer.MIN_VALUE) {
-                        bag[j] = Math.max(bag[j], bag[j-jewel[i][0]] + jewel[i][1]);
-                    //}
+        for(int i=1;i<=n;i++){
+            //case1. i번째 보석 집어넣기.
+            //case2. i번째 보석 안 집어넣기.
+            for(int j=0;j<=m;j++){
+                if (j>= weight[i]) {
+                    dp[i][j] = Math.max(dp[i-1][j-weight[i]]+value[i], dp[i-1][j]);
+                } 
+                else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        System.out.println(bag[m]);
+        System.out.println(dp[n][m]);
+
     }
 }
