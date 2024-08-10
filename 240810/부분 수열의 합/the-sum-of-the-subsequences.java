b6bb4c2602d1fa8fd36c9956767791dd2b264ac8
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-10 15:20:39 +0900

    Title: 부분 수열의 합, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240810/부분 수열의 합/the-sum-of-the-subsequences.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int n,m;
+    private static int[] arr;
+    private static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[m+1];
+
+        for(int i=0;i<=m;i++){
+            dp[i] = Integer.MIN_VALUE;
+        }
+        dp[0] = 0;
+
+   
+        for(int i=0;i<n;i++){
+            for(int j=m;j>=0;j--){
+                if (j>= arr[i]) {
+                    if (dp[j-arr[i]] != Integer.MIN_VALUE) {
+                        dp[j] = dp[j-arr[i]]+1;
+                    }
+                }
+            }
+        }
+        System.out.println(dp[m] == Integer.MIN_VALUE ? "NO":"YES");
+
+    }
+}
