2acaad884eb5f9d067b0cbd01429319a6e4a1bff
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-10 20:29:20 +0900

    Title: 배낭 채우기, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240810/배낭 채우기/knapsack.java
+++ 240810/배낭 채우기/knapsack.java
@@ -13,21 +13,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        //보석 weight, value 알려주기
         n = Integer.parseInt(st.nextToken());
-        // 총 무게 == 원하는 무게.
         m = Integer.parseInt(st.nextToken());
 
         weight = new int[n+1];
         value = new int[n+1];
-
         for(int i=1;i<=n;i++){
             st = new StringTokenizer(br.readLine());
             weight[i] = Integer.parseInt(st.nextToken());
             value[i] = Integer.parseInt(st.nextToken());
         }
 
-        //dp만들기
         dp = new int[n+1][m+1];
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
@@ -37,23 +33,17 @@ public class Main {
         dp[0][0] = 0;
 
         for(int i=1;i<=n;i++){
-            //case1. i번째 보석 집어넣기.
-            //case2. i번째 보석 안 집어넣기.
             for(int j=0;j<=m;j++){
-                if (j>= weight[i]) {
-                    dp[i][j] = Math.max(dp[i-1][j-weight[i]]+value[i], dp[i-1][j]);
-                } 
+                if (j >= weight[i]) {
+                    dp[i][j] = Math.max(dp[i-1][j-weight[i]]+value[i],
+                    dp[i-1][j]);
+                }
                 else{
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
-          int ans = 0;
-
-        for(int j = 0; j <= m; j++)
-            ans = Math.max(ans, dp[n][j]);
-
-        System.out.print(ans);
-
+                            
+        System.out.println(dp[n][m]);
     }
 }
