d2ba067abbd74bd4b603ddb462c1ca5a68abc30b
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-10 15:21:13 +0900

    Title: 부분 수열의 합, Time: 114ms, Memory: 9MB, Status: Passed - CodeTree

--- 240810/부분 수열의 합/the-sum-of-the-subsequences.java
+++ 240810/부분 수열의 합/the-sum-of-the-subsequences.java
@@ -37,7 +37,7 @@ public class Main {
                 }
             }
         }
-        System.out.println(dp[m] == Integer.MIN_VALUE ? "NO":"YES");
+        System.out.println(dp[m] == Integer.MIN_VALUE ? "No":"Yes");
 
     }
 }
