63d246ac2ac5c03566b04b0c78fb1aea31937045
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-18 21:09:50 +0900

    Title: 최대 합 분할, Time: 340ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240818/최대 합 분할/maximum-sum-partition.java
+++ 240818/최대 합 분할/maximum-sum-partition.java
@@ -4,50 +4,58 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final int OFFSET = 100000;
-    private static final int MAX_N = 100;
-    private static int n, m;
+    private static final int offset = 1000000;
+    private static int n;
     private static int[] arr;
     private static int[][] dp;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1];
+        arr = new int[n+1];
 
+        int sum = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+        for(int i=1;i<=n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            m += arr[i];
+            sum += arr[i];
         }
-
-        dp = new int[n + 1][2 * OFFSET + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = -m; j <= m; j++) {
-                dp[i][j + OFFSET] = Integer.MIN_VALUE;
+        
+        dp = new int[n+1][2*offset+1];
+        for(int i=0;i<=n;i++){
+            for(int j=-sum;j<=sum;j++){
+                dp[i][j+offset] = Integer.MIN_VALUE;
             }
         }
-        dp[0][OFFSET] = 0;
+        dp[0][offset] = 0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = -m; j <= m; j++) {
-                // 그룹 A에 i번째 원소를 추가하는 경우
-                if (j - arr[i] >= -m && j - arr[i] <= m && dp[i - 1][j - arr[i] + OFFSET] != Integer.MIN_VALUE) {
-                    dp[i][j + OFFSET] = Math.max(dp[i][j + OFFSET], dp[i - 1][j - arr[i] + OFFSET] + arr[i]);
+        for(int i=1;i<=n;i++){
+            for(int j=-sum; j<=sum; j++){
+                // a에 들어가는 경우 -> a-b가 증가.
+                // 이전 상황은 j-arr[i]인 경우
+                if (j-arr[i] >= -sum && j-arr[i] <= sum
+                    && dp[i-1][j+offset-arr[i]] != Integer.MIN_VALUE) {
+                
+                        dp[i][j+offset] = Math.max(dp[i][j+offset], dp[i-1][j+offset-arr[i]] + arr[i]);
                 }
 
-                // 그룹 B에 i번째 원소를 추가하는 경우
-                if (j + arr[i] >= -m && j + arr[i] <= m && dp[i - 1][j + arr[i] + OFFSET] != Integer.MIN_VALUE) {
-                    dp[i][j + OFFSET] = Math.max(dp[i][j + OFFSET], dp[i - 1][j + arr[i] + OFFSET]);
+                // b에 들어가는 경우 -> a-b 감소
+                // 이전 상황은 j+arr[i]의 상황, 여기까지오면 이미 a-b감소상태
+                if (j+arr[i] >= -sum && j+arr[i] <=sum
+                    && dp[i-1][j+offset+arr[i]] != Integer.MIN_VALUE) {
+                    
+                        dp[i][j+offset] = Math.max(dp[i][j+offset] , dp[i-1][j+offset+arr[i]]);
                 }
 
-                // 그룹 C에 i번째 원소를 추가하는 경우
-                if (dp[i - 1][j + OFFSET] != Integer.MIN_VALUE) {
-                    dp[i][j + OFFSET] = Math.max(dp[i][j + OFFSET], dp[i - 1][j + OFFSET]);
+                // c에 들어가는 경우
+                // 값은 변경되는게 아니고 이전의 j값이 그대로온다.
+                if (dp[i-1][j+offset] != Integer.MIN_VALUE) {
+                    dp[i][j+offset] = Math.max(dp[i][j+offset], dp[i-1][j+offset]);
                 }
             }
         }
 
-        System.out.println(dp[n][OFFSET]);
+        System.out.println(dp[n][offset]);
+
     }
 }
