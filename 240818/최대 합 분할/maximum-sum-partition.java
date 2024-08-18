886c95aef1cf4aeaf2ba93a50a6dd82f751a1046
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-18 16:48:19 +0900

    Title: 최대 합 분할, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240818/최대 합 분할/maximum-sum-partition.java
+++ 240818/최대 합 분할/maximum-sum-partition.java
@@ -6,7 +6,8 @@ import java.util.StringTokenizer;
 public class Main {
     private static int n;
     private static int[] arr;
-    private static int[][] dp;
+    private static boolean[][] dp;
+
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -15,28 +16,35 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int sum = 0;
-        for(int i=1;i<=n;i++){
+        for(int i=1; i<=n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
             sum += arr[i];
         }
 
-        int halfSum = sum /2;
-        dp = new int[n+1][halfSum+1];
+        // DP 테이블 초기화
+        int halfSum = sum / 2;
+        dp = new boolean[n+1][halfSum+1];
+        dp[0][0] = true;  // 0개의 숫자로 합 0을 만드는 것은 항상 가능
 
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=halfSum;j++){
-                // 이전 상태의 값 -> 현재숫자를 사용하지 않는 경우
-                dp[i][j] = dp[i-1][j];
-
+        for(int i=1; i<=n; i++) {
+            for(int j=halfSum; j>=0; j--) {
                 if (j >= arr[i]) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-arr[i]] + arr[i]);
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        // 최종 결과 찾기
-        int ans = dp[n][halfSum];  // 마지막 행에서 최대의 값 찾기
+
+        // 가능한 최대의 동일한 합 찾기
+        int ans = 0;
+        for (int j = halfSum; j >= 0; j--) {
+            if (dp[n][j]) {
+                ans = j;
+                break;
+            }
+        }
 
         System.out.println(ans);
-
     }
 }
