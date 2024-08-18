bd381a3863acfdd0fd06c90d315d9a3297b3276d
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-18 17:32:01 +0900

    Title: 최대 합 분할, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240818/최대 합 분할/maximum-sum-partition.java
+++ 240818/최대 합 분할/maximum-sum-partition.java
@@ -4,44 +4,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int n;
-    private static int[] arr;
-    private static boolean[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            sum += arr[i];
+            totalSum += arr[i];
         }
 
-        int halfSum = sum / 2;
-        dp = new boolean[n + 1][halfSum + 1];
+        int halfSum = totalSum / 2;
+        boolean[][] dp = new boolean[n + 1][halfSum + 1];
         dp[0][0] = true;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = halfSum; j >= 0; j--) {
-                if (j >= arr[i]) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+            int current = arr[i - 1];
+            for (int j = halfSum; j >= current; j--) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - current];
+            }
+            for (int j = 0; j < current; j++) {
+                dp[i][j] = dp[i - 1][j];
             }
         }
 
-        int ans = 0;
-        for (int i = halfSum; i >= 0; i--) {
-            if (dp[n][i]) {
-                ans = i;
+        int result = 0;
+        for (int j = halfSum; j >= 0; j--) {
+            if (dp[n][j]) {
+                result = j;
                 break;
             }
         }
-        System.out.println(ans);
+
+        System.out.println(result);
     }
 }
