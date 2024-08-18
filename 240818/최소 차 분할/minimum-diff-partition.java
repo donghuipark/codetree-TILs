373dd8f9f469737f1f48a40fc0e93be488cd072a
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-18 22:38:02 +0900

    Title: 최소 차 분할, Time: 151ms, Memory: 13MB, Status: Passed - CodeTree

--- 240818/최소 차 분할/minimum-diff-partition.java
+++ 240818/최소 차 분할/minimum-diff-partition.java
@@ -4,45 +4,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int n;
-    private static int[] number;
+    private static int n, m;
+    private static int[] arr;
     private static boolean[][] dp;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
         n = Integer.parseInt(br.readLine());
-        number = new int[n+1];
+        arr = new int[n+1];
 
-        st = new StringTokenizer(br.readLine());
-        int sum = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=1;i<=n;i++){
-            number[i] = Integer.parseInt(st.nextToken());
-            sum += number[i];
+            arr[i] = Integer.parseInt(st.nextToken());
+            m += arr[i];
         }
 
-        dp = new boolean[n+1][sum+1];
+
+        dp = new boolean[n+1][m+1];
         dp[0][0] = true;
 
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if (j >= number[i] && dp[i-1][j-number[i]]) {
-                    dp[i][j] = true;            
-                }
-
-                if (dp[i-1][j]) {
+            for(int j=0;j<=m;j++){
+                if (j>= arr[i] && dp[i-1][j-arr[i]]) {
+                    dp[i][j] = true;
+                }                
+                if(dp[i-1][j]){
                     dp[i][j] = true;
                 }
             }
         }
 
         int ans = Integer.MAX_VALUE;
-        for(int i=1;i<sum;i++){
+        for(int i=0;i<=m;i++){
             if (dp[n][i]) {
-                ans = Math.min(ans, Math.abs(i - (sum-i)));
+                ans = Math.min(ans, Math.abs(i - (m-i)));
             }
+
         }
         System.out.println(ans);
-       }
+
+
+    }
 }
