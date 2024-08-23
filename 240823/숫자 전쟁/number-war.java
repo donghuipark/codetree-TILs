81af01b8e869d600522fc66908e62e830423274f
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-23 17:28:48 +0900

    Title: 숫자 전쟁, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240823/숫자 전쟁/number-war.java
+++ 240823/숫자 전쟁/number-war.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,39 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 카드의 개수 n 입력
         int n = scanner.nextInt();
-        
-        // 첫 번째 플레이어의 카드 배열 입력
         int[] player1 = new int[n];
+        int[] player2 = new int[n];
+
         for (int i = 0; i < n; i++) {
             player1[i] = scanner.nextInt();
         }
 
-        // 두 번째 플레이어의 카드 배열 입력
-        int[] player2 = new int[n];
         for (int i = 0; i < n; i++) {
             player2[i] = scanner.nextInt();
         }
 
-        // 카드들을 정렬
-        Arrays.sort(player1);
-        Arrays.sort(player2);
+        int[][] dp = new int[n + 1][n + 1];
 
-        int score = 0;
-        int i = 0, j = 0;
-
-        // 두 플레이어의 카드를 비교하며 남우가 이길 수 있는 경우 최대한 점수를 얻도록 함
-        while (i < n && j < n) {
-            if (player2[j] < player1[i]) {
-                score += player2[j];
-                j++;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j + 1];
+                if (player1[i] > player2[j]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][j + 1] + player2[j]);
+                }
+                dp[i][j] = Math.max(dp[i][j], dp[i + 1][j]);
             }
-            i++;
         }
 
-        // 결과 출력
-        System.out.println(score);
+        System.out.println(dp[0][0]);
 
         scanner.close();
     }
