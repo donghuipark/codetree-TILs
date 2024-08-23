b0f31b7d17802f3e4ad1d4eef4507b4e27ed59f5
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-08-23 17:29:14 +0900

    Title: 숫자 전쟁, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240823/숫자 전쟁/number-war.java
+++ 240823/숫자 전쟁/number-war.java
@@ -17,15 +17,22 @@ public class Main {
             player2[i] = scanner.nextInt();
         }
 
+        // DP 테이블 초기화
         int[][] dp = new int[n + 1][n + 1];
 
+        // DP로 최대 점수 계산
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                dp[i][j] = dp[i + 1][j + 1];
-                if (player1[i] > player2[j]) {
+                // 카드를 버리는 경우
+                dp[i][j] = dp[i + 1][j];
+                
+                // 남우의 카드가 상대의 카드보다 클 경우
+                if (player2[j] > player1[i]) {
                     dp[i][j] = Math.max(dp[i][j], dp[i][j + 1] + player2[j]);
                 }
-                dp[i][j] = Math.max(dp[i][j], dp[i + 1][j]);
+
+                // 그 외의 경우
+                dp[i][j] = Math.max(dp[i][j], dp[i + 1][j + 1]);
             }
         }
 
