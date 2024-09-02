2dc1d3a475788a6aa475934863942c74af11140b
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-09-02 18:59:20 +0900

    Title: k번째로 작은 쌍의 합, Time: 1359ms, Memory: 53MB, Status: Passed - CodeTree

--- 240902/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.java
+++ 240902/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.java
@@ -1,59 +1,72 @@
 import java.util.*;
-import java.io.*;
 
-class Pair implements Comparable<Pair>{
-    int x, y;
+class Pair implements Comparable<Pair> {
+    int arr1Idx;
+    int arr2Idx;
+    int sum;
 
-    public Pair(int x, int y ){
-        this.x = x;
-        this.y = y;
+    public Pair(int arr1Idx, int arr2Idx, int sum) {
+        this.arr1Idx = arr1Idx;
+        this.arr2Idx = arr2Idx;
+        this.sum = sum;
     }
 
     @Override
-    public int compareTo(Pair p){
-        return (this.x + this.y) - (p.x+p.y);
+    public int compareTo(Pair other) {
+        return Integer.compare(this.sum, other.sum);
     }
 }
 
 public class Main {
-    private static int n, m, k;
-    private static int[] arr;
-    private static int[] arr2;
-    public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        arr = new int[n];
-
-        m = Integer.parseInt(st.nextToken());
-        arr2 = new int[m];
-
-        k = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int k = scanner.nextInt();
+        
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[m];
+        
+        for (int i = 0; i < n; i++) {
+            arr1[i] = scanner.nextInt();
         }
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<m;i++){
-            arr2[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) {
+            arr2[i] = scanner.nextInt();
         }
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                pq.add(new Pair(arr[i], arr2[j]));
+        
+        System.out.println(kthSmallestPairSum(n, m, k, arr1, arr2));
+        
+        scanner.close();
+    }
+    
+    public static int kthSmallestPairSum(int n, int m, int k, int[] arr1, int[] arr2) {
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>();
+        
+        // 첫 번째 배열의 첫 번째 원소와 두 번째 배열의 모든 원소의 합을 힙에 추가
+        for (int i = 0; i < m; i++) {
+            minHeap.offer(new Pair(0, i, arr1[0] + arr2[i]));
+        }
+        
+        int count = 0;
+        int result = 0;
+        
+        while (count < k) {
+            Pair curr = minHeap.poll();
+            result = curr.sum;
+            int arr1Idx = curr.arr1Idx;
+            int arr2Idx = curr.arr2Idx;
+            count++;
+            
+            // 첫 번째 배열의 다음 원소와 두 번째 배열의 현재 원소의 합을 힙에 추가
+            if (arr1Idx + 1 < n) {
+                minHeap.offer(new Pair(arr1Idx + 1, arr2Idx, arr1[arr1Idx + 1] + arr2[arr2Idx]));
             }
         }
-        int ans = 0;
-        for(int i=0;i<k;i++){
-            Pair res = pq.poll();
-            ans = res.x + res.y;
-        }
-    
-        System.out.println(ans);
+        
+        return result;
     }
 }
