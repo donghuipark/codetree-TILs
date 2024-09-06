ad7e95d3d9a6d823980632867867b4c9c0955fcd
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-09-06 22:43:18 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240906/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int n;
+    private static int[] arr;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int ans = Integer.MIN_VALUE;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                ans = Math.max(ans, arr[j] - arr[i]);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
