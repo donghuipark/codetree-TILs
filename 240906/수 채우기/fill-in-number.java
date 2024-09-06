ff96fd89043d69ee44158fe0cab42816632beae3
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-09-06 22:13:19 +0900

    Title: 수 채우기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240906/수 채우기/fill-in-number.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int n;
+
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        n = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        while(n >= 5){
+            ans++;
+            n -= 5;
+        }
+        while(n >= 2){
+            ans++;
+            n -= 2;
+        }
+
+        if(n != 0){
+            System.out.println(-1);
+        }
+        else{
+            System.out.println(ans);
+        }
+    }
+}
