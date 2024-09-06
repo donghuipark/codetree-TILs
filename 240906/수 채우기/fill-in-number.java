9d65edaa52a5521864171c6fcb26b832ec01429d
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-09-06 22:36:36 +0900

    Title: 수 채우기, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

--- 240906/수 채우기/fill-in-number.java
+++ 240906/수 채우기/fill-in-number.java
@@ -1,21 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        
-        System.out.println(getMiniCoins(n));
-    }
+public class Main{
 
-    public static int getMiniCoins(int n) {
-        // 5원 동전 최대 개수를 구합니다.
-        for (int i = n / 5; i >= 0; i--) {
-            int remaining = n - (i * 5);  // 5원 동전을 사용한 후 남은 금액
-            if (remaining % 2 == 0) {  // 남은 금액이 2원 동전으로 나누어 떨어지면
-                return i + (remaining / 2);  // 5원 동전 개수 + 2원 동전 개수
+    private static int n;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        int ans = -1;
+        for(int i = n/5; i>=0 ; i--){
+            int remain = n - (5*i);
+            if(remain%2 == 0){
+                ans = i + (remain/2);
+                break;
             }
         }
-        return -1;  // 정확히 맞출 수 없는 경우
+        System.out.println(ans);
     }
 }
