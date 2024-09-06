c826a59282c98b7976f18db35bd06cd5d40ee1be
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-09-06 22:20:35 +0900

    Title: 수 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240906/수 채우기/fill-in-number.java
+++ 240906/수 채우기/fill-in-number.java
@@ -3,28 +3,42 @@ import java.io.*;
 
 public class Main {
     private static int n;
-
+    private static int[] dp;
     public static void main(String[] args) throws IOException{
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         n = Integer.parseInt(br.readLine());
+        dp = new int[n+1];
 
-        int ans = 0;
-        while(n >= 5){
-            ans++;
-            n -= 5;
+        for(int i=0;i<=n;i++){
+            dp[i] = -1;
         }
-        while(n >= 2){
-            ans++;
-            n -= 2;
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = -1;
+        dp[4] = 2;
+        dp[5] = 1;
+
+        for(int i=6;i<=n;i++){
+            if(dp[i-2] == -1){
+                if(dp[i-5] != -1){
+                    dp[i] = dp[i-5] + 1;
+                }
+            }
+            else{
+                if(dp[i-5] != -1){
+                    dp[i] = Math.min(dp[i-2] +1, dp[i-5]+1);
+                }
+                else{
+                    dp[i] = dp[i-2]+1;
+                }
+            }
         }
 
-        if(n != 0){
-            System.out.println(-1);
-        }
-        else{
-            System.out.println(ans);
-        }
+        System.out.println(dp[n]);
+
+        
     }
 }
