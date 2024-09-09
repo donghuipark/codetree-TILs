f86c68e7b3272e2000f31aa773e0e9d0920d9d2a
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-09-09 20:15:24 +0900

    Title: 자연수 n개의 합, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240909/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 240909/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -4,35 +4,33 @@ import java.io.*;
 public class Main {
     private static long s;
 
-    private static long binarySearch(long s){
+
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        s = Long.parseLong(br.readLine());
+
         long left = 1;
         long right = s;
         long maxIdx = -1;
 
-        while(left <= right){
+        while(left<= right){
             long mid = (left + right) / 2;
-            // 오버플로우를 방지하기 위한 조건
-            if(mid > 0 && mid <= Long.MAX_VALUE / (mid + 1) / 2) {
-                long sum = mid * (mid + 1) / 2;
-                if(sum <= s){
-                    left = mid + 1;
-                    maxIdx = mid;  // mid 값을 저장
-                } else {
-                    right = mid - 1;
-                }
-            } else {
-                right = mid - 1; // mid가 너무 커서 오버플로우가 발생할 가능성이 있을 때
+            if(mid * (mid+1) /2 <= s){
+                // left mid a  right
+                left = mid + 1;
+                maxIdx = Math.max(maxIdx, mid);
+            }
+            else{
+                // left  mid ans right
+                right = mid - 1;
             }
         }
-        return maxIdx;
-    }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        s = Long.parseLong(br.readLine());
+        System.out.println(maxIdx);    
 
-        long ans = binarySearch(s);
 
-        System.out.println(ans);
     }
 }
