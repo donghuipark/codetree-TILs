e72a8d721265b0c81b98d44a8569b3b7510e183c
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-09-09 21:00:33 +0900

    Title: 정수 분배하기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240909/정수 분배하기/distributing-integers.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int n,m;
+    private static int[] arr;
+
+    private static boolean distribute(int mid){
+        int cnt = 0;
+
+        for(int i=0;i<n;i++){
+            int num = arr[i];
+            while(num >= mid){
+                cnt++;
+                num -= mid;
+            }
+        }
+
+        if(cnt >= m){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        int right = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            right = Math.min(right, arr[i]);
+        }
+
+        int left = 1;
+        int ans = Integer.MIN_VALUE;
+
+        while(left <= right){
+            int mid = (right + left) / 2;
+            if(distribute(mid)){
+                left = mid + 1;
+                ans = Math.max(ans, mid);
+            }
+            else{
+                right = mid - 1;
+            }
+        }        
+
+        System.out.println(ans);
+
+
+    }
+}
