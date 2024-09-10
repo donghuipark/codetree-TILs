844ee0027de3d72dc900fa7dfbab8edc4a414fea
donghuipark <109971743+donghuipark@users.noreply.github.com>
2024-09-10 21:25:41 +0900

    Title: 겹치지 않는 선분위의 두 점, Time: 309ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240910/겹치지 않는 선분위의 두 점/two-points-of-the-line-that-dont-overlap.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    private static int n, m;
+    private static long[][] arr;
+
+    private static long simulate(long distance){
+        long cnt = 1;
+        long lastPlaced = arr[0][0];
+
+        for(int i=0;i<m;i++){
+            long start = arr[i][0];
+            long end = arr[i][1];
+
+            while(lastPlaced + distance <= end){
+                cnt++;
+                lastPlaced += distance;
+
+                if(cnt >= n){
+                    return cnt;
+                }
+            }
+
+            if(lastPlaced < start){
+                lastPlaced = start;
+            }
+        }
+        return cnt;
+    }
+    private static long binarySearch(int target){
+        long left = 1;
+        long right = arr[m-1][1] - arr[0][0];
+        long ans = -1;
+        while(left <= right){
+            long mid = (right+left) / 2;
+
+            if(simulate(mid) >= target){
+                ans = mid;
+                left = mid + 1;
+               
+            }
+            else{
+                right = mid - 1;
+                
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new long[m][2];
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            long s = Integer.parseInt(st.nextToken());
+            long e = Integer.parseInt(st.nextToken());
+
+            arr[i][0] = s;
+            arr[i][1] = e;
+        }
+
+        Arrays.sort(arr, (o1, o2) -> Long.compare(o1[0], o2[0]));
+            
+
+        System.out.println(binarySearch(n));
+
+    }
+}
